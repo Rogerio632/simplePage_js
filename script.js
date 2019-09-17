f8732c2bc50ad0943ee3c7240f3eac9088e11042
Rogério D. Oliveira <48360931+Rogerio632@users.noreply.github.com>
2019-09-17 17:56:49 -0300

    add script.js and some new things

---
+++ script.js
@@ -0,0 +1,18 @@
+let consulta = document.querySelectorAll(`.modal-body`) //[1]
+
+/*elementos*/
+let nome = document.getElementById(`nome`)
+let descricao = document.getElementById(`descricao`)
+let foto = document.getElementById(`imagem`)
+
+let cadastrar = document.getElementById(`cadastrar`)
+
+cadastrar.addEventListener(`click`, () => {
+
+/*A fazer*/
+
+
+
+})
+
+
